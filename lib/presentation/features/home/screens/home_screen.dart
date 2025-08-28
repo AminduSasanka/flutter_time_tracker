57dac803003d68e294970e42aa232488f2a56e97
Amindu Liyanaratne <reservedchery@gmail.com>
2025-08-28 20:43:04 +0530

    added home page scaffold

--- lib/presentation/features/home/screens/home_screen.dart
+++ lib/presentation/features/home/screens/home_screen.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_time_tracker/presentation/features/home/widgets/settings_widget.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: const SettingsWidget(),
+      bottomNavigationBar: const Placeholder(
+        child: Text("Bottom Navigation Bar"),
+      ),
+    );
   }
 }
