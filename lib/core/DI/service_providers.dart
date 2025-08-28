36ec8983e76c68c1da0a2063de12756353a51493
Amindu Liyanaratne <reservedchery@gmail.com>
2025-08-28 22:56:09 +0530

    changed jira service provider name

--- lib/core/DI/service_providers.dart
+++ lib/core/DI/service_providers.dart
@@ -2,6 +2,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_time_tracker/application/services/jira_auth_service.dart';
 import 'package:flutter_time_tracker/core/DI/repository_providers.dart';
 
-final apiTokenServiceProvider = Provider<JiraAuthService>((ref) {
+final jiraAuthServiceProvider = Provider<JiraAuthService>((ref) {
   return JiraAuthService(ref.read(jiraAuthRepositoryProvider));
 });
