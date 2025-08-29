8255bbf27dcd329336b78010a3d35ea2d66c9838
Amindu Liyanaratne <reservedchery@gmail.com>
2025-08-29 20:25:37 +0530

    changed layouts to use main layout

--- lib/core/DI/routes_provider.dart
+++ lib/core/DI/routes_provider.dart
@@ -4,31 +4,39 @@ import 'package:flutter_time_tracker/presentation/features/add_worklog/screens/a
 import 'package:flutter_time_tracker/presentation/features/history/screens/history_screen.dart';
 import 'package:flutter_time_tracker/presentation/features/home/screens/home_screen.dart';
 import 'package:flutter_time_tracker/presentation/features/settings/screens/settings_screen.dart';
+import 'package:flutter_time_tracker/presentation/shared/layouts/main_layout.dart';
 import 'package:go_router/go_router.dart';
 
 final goRouterProvider = Provider<GoRouter>((ref) {
   return GoRouter(
     initialLocation: '/',
     routes: [
-      GoRoute(
-        path: '/',
-        name: homeRoute,
-        builder: (context, state) => const HomeScreen(),
+      ShellRoute(
+        builder: (context, state, child) {
+          return MainLayout(child: child);
+        },
+        routes: [
+          GoRoute(
+            path: '/',
+            name: homeRoute,
+            builder: (context, state) => const HomeScreen(),
+          ),
+          GoRoute(
+            path: '/history',
+            name: historyRoute,
+            builder: (context, state) => const HistoryScreen(),
+          ),
+          GoRoute(
+            path: '/add_worklog',
+            name: addWorklogRoute,
+            builder: (context, state) => const AddWorklogScreen(),
+          ),
+        ],
       ),
       GoRoute(
         path: '/settings',
         name: settingsRoute,
         builder: (context, state) => const SettingsScreen(),
-      ),
-      GoRoute(
-        path: '/history',
-        name: historyRoute,
-        builder: (context, state) => const HistoryScreen(),
-      ),
-      GoRoute(
-        path: '/add_worklog',
-        name: addWorklogRoute,
-        builder: (context, state) => const AddWorklogScreen(),
       ),
     ],
   );
