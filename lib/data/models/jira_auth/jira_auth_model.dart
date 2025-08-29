079bd3b6ddde4ac271fc12a71949a903a37add4c
Amindu Liyanaratne <reservedchery@gmail.com>
2025-08-30 01:39:44 +0530

    temp commit - removed freezed model and used manual model

--- lib/data/models/jira_auth/jira_auth_model.dart
+++ lib/data/models/jira_auth/jira_auth_model.dart
@@ -1,15 +1,36 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
+// import 'package:freezed_annotation/freezed_annotation.dart';
+//
+// part 'jira_auth_model.freezed.dart';
+// part 'jira_auth_model.g.dart';
+//
+// @freezed
+// class JiraAuthModel with _$JiraAuthModel {
+//   factory JiraAuthModel({
+//     @JsonKey(name: 'apiToken') required String apiToken,
+//     @JsonKey(name: 'email') required String email,
+//     @JsonKey(name: 'workspace') required String workspace,
+//   }) = _JiraAuthModel;
+//
+//   factory JiraAuthModel.fromJson(Map<String, dynamic> json) => _$JiraAuthModelFromJson(json);
+// }
 
-part 'jira_auth_model.freezed.dart';
-part 'jira_auth_model.g.dart';
 
-@Freezed()
-class JiraAuthModel with _$JiraAuthModel {
-  const factory JiraAuthModel({
-    @JsonKey(name: 'apiToken') required String apiToken,
-    @JsonKey(name: 'email') required String email,
-    @JsonKey(name: 'workspace') required String workspace,
-  }) = _JiraAuthModel;
+class JiraAuthModel {
+  final String apiToken;
+  final String email;
+  final String workspace;
 
-  factory JiraAuthModel.fromJson(Map<String, dynamic> json) => _$JiraAuthModelFromJson(json);
+  JiraAuthModel({required this.workspace, required this.email, required this.apiToken});
+
+  factory JiraAuthModel.fromJson(Map<String, dynamic> json) {
+    return JiraAuthModel(apiToken: json['apiToken'], email: json['email'], workspace: json['workspace']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "workspace": workspace,
+      "email": email,
+      "apiToken": apiToken
+    };
+  }
 }
