2d84b5cb7554b0f05ae65f08b6cda04fb0639baa
Amindu Liyanaratne <reservedchery@gmail.com>
2025-08-29 19:21:12 +0530

    added jira auth widget

--- lib/presentation/features/settings/screens/settings_screen.dart
+++ lib/presentation/features/settings/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_time_tracker/presentation/features/settings/widgets/jira_auth_widget.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -12,7 +13,7 @@ class _SettingsState extends State<SettingsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Settings")),
-      body: Center(child: Text("Settings page")),
+      body: Column(children: [JiraAuthWidget()]),
     );
   }
 }
---
+++ lib/presentation/features/settings/widgets/jira_auth_widget.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_time_tracker/core/DI/controller_providers.dart';
+import 'package:flutter_time_tracker/domain/entities/JiraAuth/JiraAuth.dart';
+
+class JiraAuthWidget extends ConsumerStatefulWidget {
+  const JiraAuthWidget({super.key});
+
+  @override
+  ConsumerState<JiraAuthWidget> createState() => _JiraAuthWidgetState();
+}
+
+class _JiraAuthWidgetState extends ConsumerState<JiraAuthWidget> {
+  final _emailController = TextEditingController();
+  final _workspaceController = TextEditingController();
+  final _tokenController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _workspaceController.dispose();
+    _tokenController.dispose();
+    super.dispose();
+  }
+
+  void _updateJiraAuth() async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        await ref
+            .read(settingsPageControllerProvider.notifier)
+            .update(
+              _emailController.text,
+              _workspaceController.text,
+              _tokenController.text,
+            );
+      } catch (e) {
+        if (mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Update Failed: ${e.toString()}')),
+          );
+        }
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool isLoading = ref.watch(settingsPageControllerProvider).isLoading;
+    JiraAuth jiraAuth = ref.watch(settingsPageControllerProvider).jiraAuth;
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextFormField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+              initialValue: jiraAuth.email,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your jira email';
+                }
+
+                return null;
+              },
+            ),
+            const SizedBox(height: 16),
+            TextFormField(
+              controller: _workspaceController,
+              decoration: const InputDecoration(labelText: 'Workspace'),
+              initialValue: jiraAuth.workspace,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your jira workspace url';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 16),
+            TextFormField(
+              controller: _tokenController,
+              decoration: const InputDecoration(labelText: 'API Token'),
+              initialValue: jiraAuth.apiToken,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your API token';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 24),
+            isLoading
+                ? const CircularProgressIndicator()
+                : ElevatedButton(
+                    onPressed: _updateJiraAuth,
+                    child: const Text('Update Credentials'),
+                  ),
+          ],
+        ),
+      ),
+    );
+  }
+}
