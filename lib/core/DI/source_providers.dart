f3d78af959c9150855ea489fc81fe1645d439478
Amindu Liyanaratne <reservedchery@gmail.com>
2025-08-30 22:10:49 +0530

    added jira api service provider

--- lib/core/DI/source_providers.dart
+++ lib/core/DI/source_providers.dart
@@ -1,7 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_time_tracker/core/DI/repository_providers.dart';
+import 'package:flutter_time_tracker/core/services/network/network_service_provider.dart';
 import 'package:flutter_time_tracker/data/sources/local/secure_storage/i_secure_storage_service.dart';
 import 'package:flutter_time_tracker/data/sources/local/secure_storage/secure_storage_service.dart';
+import 'package:flutter_time_tracker/data/sources/remote/jira/i_jira_api_service.dart';
+import 'package:flutter_time_tracker/data/sources/remote/jira/jira_api_service.dart';
 
 final secureStorageProvider = Provider<FlutterSecureStorage>((ref) {
   const AndroidOptions androidOptions = AndroidOptions(
@@ -20,4 +24,8 @@ final secureStorageProvider = Provider<FlutterSecureStorage>((ref) {
 
 final secureStorageServiceProvider = Provider<ISecureStorageService>((ref) {
   return SecureStorageService(ref.watch(secureStorageProvider));
+});
+
+final jiraApiServiceProvider = Provider<IJiraApiService>((ref) {
+  return JiraApiService(ref.read(networkServiceProvider), ref.read(jiraAuthRepositoryProvider));
 });
