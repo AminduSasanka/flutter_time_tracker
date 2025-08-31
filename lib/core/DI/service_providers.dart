7ac2d6bee2a50895e5227b8de31739d32a3676bc
Amindu Liyanaratne <reservedchery@gmail.com>
2025-08-31 23:03:27 +0530

    fixed using concrete class instead of interface

--- lib/core/DI/service_providers.dart
+++ lib/core/DI/service_providers.dart
@@ -2,9 +2,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_time_tracker/application/services/jira_auth_service.dart';
 import 'package:flutter_time_tracker/application/services/work_log_service.dart';
 import 'package:flutter_time_tracker/core/DI/repository_providers.dart';
+import 'package:flutter_time_tracker/domain/services/i_jira_auth_service.dart';
 import 'package:flutter_time_tracker/domain/services/i_work_log_service.dart';
 
-final jiraAuthServiceProvider = Provider<JiraAuthService>((ref) {
+final jiraAuthServiceProvider = Provider<IJiraAuthService>((ref) {
   return JiraAuthService(ref.read(jiraAuthRepositoryProvider));
 });
 
