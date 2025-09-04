e0b7be3ed18e1da4924cd7a5812c2fe5d507dbb3
Amindu Liyanaratne <reservedchery@gmail.com>
2025-09-04 16:46:03 +0530

    modified to use filtering function in controller

--- lib/presentation/features/history/controller/history_screen_controller.dart
+++ lib/presentation/features/history/controller/history_screen_controller.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_time_tracker/core/DI/service_providers.dart';
+import 'package:flutter_time_tracker/core/constants/enums.dart';
 import 'package:flutter_time_tracker/domain/entities/work_log.dart';
 import 'package:flutter_time_tracker/presentation/features/history/state/history_screen_state.dart';
 
@@ -9,7 +10,9 @@ class HistoryScreenController
     extends AutoDisposeAsyncNotifier<HistoryScreenState> {
   @override
   Future<HistoryScreenState> build() async {
-    List<WorkLog> workLogs = await _getWorkLogs();
+    List<WorkLog> workLogs = await _getInitialWorkLogs(
+      state: WorkLogStateEnum.completed,
+    );
 
     return HistoryScreenState(
       workLogs: workLogs,
@@ -40,10 +43,18 @@ class HistoryScreenController
     }
   }
 
-  Future<List<WorkLog>> _getWorkLogs({DateTime? startDate, DateTime? endDate}) async {
+  Future<List<WorkLog>> _getInitialWorkLogs({
+    WorkLogStateEnum? state,
+    DateTime? startDate,
+    String? taskKey,
+  }) async {
     final result = await ref
         .read(workLogServiceProvider)
-        .getCompletedWorkLogs();
+        .getFilteredWorkLogs(
+          state: state,
+          startDate: startDate,
+          taskKey: taskKey,
+        );
 
     if (result.isSuccess()) {
       List<WorkLog>? workLogs = result.tryGetSuccess();
