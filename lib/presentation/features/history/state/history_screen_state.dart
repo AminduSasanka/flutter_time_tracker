e4067c7243db30637f178836b2a771f6c247e602
Amindu Liyanaratne <reservedchery@gmail.com>
2025-09-04 13:03:50 +0530

    added history screen state

---
+++ lib/presentation/features/history/state/history_screen_state.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_time_tracker/domain/entities/work_log.dart';
+
+class HistoryScreenState {
+  List<WorkLog> workLogs = [];
+  bool isError = false;
+  String? errorMessage;
+  DateTime? filterStartDate;
+  DateTime? filterEndDate;
+
+  HistoryScreenState({
+    required this.workLogs,
+    required this.isError,
+    required this.errorMessage,
+    required this.filterStartDate,
+    required this.filterEndDate,
+  });
+
+  HistoryScreenState copyWith({
+    List<WorkLog>? workLogs,
+    bool? isError,
+    String? errorMessage,
+    DateTime? filterStartDate,
+    DateTime? filterEndDate,
+  }) {
+    return HistoryScreenState(
+      workLogs: workLogs ?? this.workLogs,
+      isError: isError ?? this.isError,
+      errorMessage: errorMessage ?? this.errorMessage,
+      filterStartDate: filterStartDate ?? this.filterStartDate,
+      filterEndDate: filterEndDate ?? this.filterEndDate,
+    );
+  }
+}
