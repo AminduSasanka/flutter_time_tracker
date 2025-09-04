73a5abba59cde70704074c580213de7ba221cbcd
Amindu Liyanaratne <reservedchery@gmail.com>
2025-09-04 13:04:45 +0530

    added basic history screen

--- lib/presentation/features/history/screens/history_screen.dart
+++ lib/presentation/features/history/screens/history_screen.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_time_tracker/core/DI/controller_providers.dart';
+import 'package:flutter_time_tracker/domain/entities/work_log.dart';
 
 class HistoryScreen extends ConsumerWidget {
   const HistoryScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(title: Text("History")),
-      body: Center(child: Text("History page")),
+    final historyScreenState = ref.watch(historyScreenControllerProvider);
+
+    return historyScreenState.when(
+      data: (state) {
+        final List<WorkLog> workLogs = state.workLogs;
+
+        return Scaffold(
+          appBar: AppBar(title: Text("History")),
+          body: ListView.builder(
+            itemCount: workLogs.length,
+            itemBuilder: (context, index) {
+              final workLog = workLogs[index];
+              return ListTile(
+                title: Text(workLog.taskKey),
+                subtitle: Text(workLog.summary),
+              );
+            },
+          ),
+        );
+      },
+      error: (error, stack) => Text('Error: $error'),
+      loading: () => CircularProgressIndicator(),
     );
   }
 }
