6a7b852668a3086656c5ae9c6f7d0d17126dc669
Amindu Liyanaratne <reservedchery@gmail.com>
2025-09-07 23:22:04 +0530

    added add work log screen controller

---
+++ lib/presentation/features/add_worklog/controller/add_worklog_screen_controller.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_time_tracker/core/DI/service_providers.dart';
+import 'package:flutter_time_tracker/domain/entities/work_log.dart';
+import 'package:flutter_time_tracker/presentation/features/add_worklog/state/add_worklog_screen_state.dart';
+
+class AddWorklogScreenController
+    extends AutoDisposeAsyncNotifier<AddWorklogScreenState> {
+  @override
+  FutureOr<AddWorklogScreenState> build() async {
+    return AddWorklogScreenState(workLog: WorkLog.empty());
+  }
+
+  void addWorkLog({
+    required String taskKey,
+    required String summary,
+    required String description,
+    required String timeSpent,
+  }) {
+    final worklog = AsyncData(state.value!.workLog).value;
+
+    state = AsyncLoading();
+
+    ref
+        .read(workLogServiceProvider)
+        .createWorkLog(
+          worklog.copyWith(
+            taskKey: taskKey,
+            summary: summary,
+            description: description,
+            timeSpent: timeSpent,
+          ),
+        );
+
+    state = AsyncData(state.value!.copyWith(workLog: WorkLog.empty()));
+  }
+}
