121e681fb2dce654ec8664e9d0697327af0c8d0d
Amindu Liyanaratne <reservedchery@gmail.com>
2025-09-07 21:32:13 +0530

    added routes for edit work log path

--- lib/core/constants/route_names.dart
+++ lib/core/constants/route_names.dart
@@ -2,3 +2,4 @@ const String homeRoute = '/';
 const String settingsRoute = '/settings';
 const String historyRoute = '/history';
 const String addWorklogRoute = '/add_worklog';
+const String editWorklogRoute = '/edit_worklog';
