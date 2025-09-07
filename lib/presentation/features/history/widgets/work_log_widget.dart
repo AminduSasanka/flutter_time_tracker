91401c186764f074a353eaa304572ec8ba3f80dc
Amindu Liyanaratne <reservedchery@gmail.com>
2025-09-07 21:33:35 +0530

    added navigation to edit work log

--- lib/presentation/features/history/widgets/work_log_widget.dart
+++ lib/presentation/features/history/widgets/work_log_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_time_tracker/core/constants/route_names.dart';
 import 'package:flutter_time_tracker/domain/entities/work_log.dart';
+import 'package:go_router/go_router.dart';
 
 class WorkLogWidget extends ConsumerWidget {
   final WorkLog workLog;
@@ -10,42 +12,50 @@ class WorkLogWidget extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return ClipRect(
-      child: Container(
-        margin: const EdgeInsets.only(bottom: 12),
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.surface,
-          border: Border(
-            top: BorderSide(color: Colors.black12, width: 1),
-            right: BorderSide(color: Colors.black12, width: 1),
-            bottom: BorderSide(color: Colors.black12, width: 1),
-            left: BorderSide(color: Colors.blue, width: 1),
+      child: GestureDetector(
+        onTap: () {
+          context.pushNamed(
+            editWorklogRoute,
+            pathParameters: {'worklogId': workLog.id!.toString()},
+          );
+        },
+        child: Container(
+          margin: const EdgeInsets.only(bottom: 12),
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.surface,
+            border: Border(
+              top: BorderSide(color: Colors.black12, width: 1),
+              right: BorderSide(color: Colors.black12, width: 1),
+              bottom: BorderSide(color: Colors.black12, width: 1),
+              left: BorderSide(color: Colors.blue, width: 1),
+            ),
           ),
-        ),
-        child: Row(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  workLog.taskKey,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
+          child: Row(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    workLog.taskKey,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
-                ),
-                Text(
-                  workLog.summary,
-                  style: TextStyle(fontSize: 16, color: Colors.grey[600]),
-                ),
-              ],
-            ),
-            const Spacer(),
-            Text(
-              workLog.timeSpent == null ? "00:00" : workLog.timeSpent!,
-              style: const TextStyle(fontWeight: FontWeight.w500),
-            ),
-          ],
+                  Text(
+                    workLog.summary,
+                    style: TextStyle(fontSize: 16, color: Colors.grey[600]),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              Text(
+                workLog.timeSpent == null ? "00:00" : workLog.timeSpent!,
+                style: const TextStyle(fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
         ),
       ),
     );
