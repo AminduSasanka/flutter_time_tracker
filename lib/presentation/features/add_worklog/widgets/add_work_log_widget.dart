a78108a0745b5999b1fa5b1c63b3d542f523f2df
Amindu Liyanaratne <reservedchery@gmail.com>
2025-09-07 23:22:52 +0530

    added add work log widget

---
+++ lib/presentation/features/add_worklog/widgets/add_work_log_widget.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_time_tracker/core/DI/controller_providers.dart';
+import 'package:flutter_time_tracker/presentation/shared/widgets/work_log_form_widget.dart';
+
+class AddWorkLogWidget extends ConsumerStatefulWidget {
+  const AddWorkLogWidget({super.key});
+
+  @override
+  ConsumerState<AddWorkLogWidget> createState() => _AddWorkLogWidgetState();
+}
+
+class _AddWorkLogWidgetState extends ConsumerState<AddWorkLogWidget> {
+  late final TextEditingController _taskIdController;
+  late final TextEditingController _summaryController;
+  late final TextEditingController _descriptionController;
+  late final TextEditingController _spentTimeController;
+  late final TextEditingController _startDateController;
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    _taskIdController = TextEditingController();
+    _summaryController = TextEditingController();
+    _descriptionController = TextEditingController();
+    _spentTimeController = TextEditingController();
+    _startDateController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _taskIdController.dispose();
+    _summaryController.dispose();
+    _descriptionController.dispose();
+    _spentTimeController.dispose();
+    _startDateController.dispose();
+    super.dispose();
+  }
+
+  void saveWorkLog() async {
+    if (_formKey.currentState!.validate()) {
+      ref.read(addWorkLogScreenControllerProvider.notifier).addWorkLog(
+        taskKey: _taskIdController.text,
+        summary: _summaryController.text,
+        description: _descriptionController.text,
+        timeSpent: _spentTimeController.text,
+        startDate: _startDateController.text,
+      );
+
+      Navigator.pop(context);
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Work log saved successfully.')),
+      );
+    } else {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Please fill in all fields.')));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenState = ref.watch(addWorkLogScreenControllerProvider);
+
+    return screenState.when(
+      data: (state) {
+        if (_taskIdController.text.isEmpty && state.workLog.taskKey != "") {
+          _taskIdController.text = state.workLog.taskKey;
+        }
+        if (_descriptionController.text.isEmpty &&
+            state.workLog.description != null) {
+          _descriptionController.text = state.workLog.description!;
+        }
+        if (_summaryController.text.isEmpty && state.workLog.summary != "") {
+          _summaryController.text = state.workLog.summary;
+        }
+        if (_spentTimeController.text.isEmpty &&
+            state.workLog.timeSpent != null) {
+          _spentTimeController.text = state.workLog.timeSpent!;
+        }
+
+        return Padding(
+          padding: EdgeInsetsGeometry.all(15),
+          child: WorkLogFormWidget(
+            taskIdController: _taskIdController,
+            summaryController: _summaryController,
+            descriptionController: _descriptionController,
+            spentTimeController: _spentTimeController,
+            formKey: _formKey,
+            onSave: saveWorkLog,
+          ),
+        );
+      },
+      error: (error, stack) => Text('Error: $error'),
+      loading: () => CircularProgressIndicator(),
+    );
+  }
+}
