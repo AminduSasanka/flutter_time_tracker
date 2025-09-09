fcb257013bf682c392406825cf91c1146f442185
Amindu Liyanaratne <reservedchery@gmail.com>
2025-09-09 22:41:37 +0530

    changed widget to a consumer widget

--- lib/presentation/features/add_worklog/screens/add_worklog_screen.dart
+++ lib/presentation/features/add_worklog/screens/add_worklog_screen.dart
@@ -3,22 +3,97 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_time_tracker/core/DI/controller_providers.dart';
 import 'package:flutter_time_tracker/presentation/features/add_worklog/widgets/add_work_log_widget.dart';
 
-class AddWorklogScreen extends ConsumerWidget {
+class AddWorklogScreen extends ConsumerStatefulWidget {
   const AddWorklogScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-     final screenState = ref.watch(addWorkLogScreenControllerProvider);
+  ConsumerState<AddWorklogScreen> createState() => _AddWorklogScreenState();
+}
 
-    return screenState.when(
-      data: (state) {
-        return Scaffold(
-          appBar: AppBar(title: Text('Add Work Log')),
-          body: AddWorkLogWidget(),
-        );
-      },
-      error: (error, stack) => Text('Error: $error'),
-      loading: () => CircularProgressIndicator(),
+class _AddWorklogScreenState extends ConsumerState<AddWorklogScreen> {
+  late final TextEditingController _taskIdController;
+  late final TextEditingController _summaryController;
+  late final TextEditingController _descriptionController;
+  late final TextEditingController _spentTimeController;
+  late final TextEditingController _startDateController;
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    _taskIdController = TextEditingController();
+    _summaryController = TextEditingController();
+    _descriptionController = TextEditingController();
+    _spentTimeController = TextEditingController();
+    _startDateController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _taskIdController.dispose();
+    _summaryController.dispose();
+    _descriptionController.dispose();
+    _spentTimeController.dispose();
+    _startDateController.dispose();
+    super.dispose();
+  }
+
+  Future<bool> _saveWorkLog() async {
+    final isWorkLogAdded = await ref
+        .read(addWorkLogScreenControllerProvider.notifier)
+        .addWorkLog(
+      taskKey: _taskIdController.text,
+      summary: _summaryController.text,
+      description: _descriptionController.text,
+      timeSpent: _spentTimeController.text,
+      startDate: _startDateController.text,
+    );
+
+    return isWorkLogAdded;
+  }
+
+  void _clearInputs() {
+    _taskIdController.clear();
+    _summaryController.clear();
+    _descriptionController.clear();
+    _spentTimeController.clear();
+    _startDateController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenState = ref.watch(addWorkLogScreenControllerProvider);
+
+    void handleAddWorkLog() async {
+      if (_formKey.currentState!.validate()) {
+        bool isWorkLogAdded = await _saveWorkLog();
+
+        if (isWorkLogAdded) {
+          _clearInputs();
+
+          if (!context.mounted) return;
+
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Work log added successfully'),
+            ),
+          );
+        }
+      }
+    }
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Add Work Log')),
+      body: AddWorkLogWidget(
+        state: screenState,
+        taskIdController: _taskIdController,
+        summaryController: _summaryController,
+        descriptionController: _descriptionController,
+        spentTimeController: _spentTimeController,
+        startDateController: _startDateController,
+        formKey: _formKey,
+        onSave: handleAddWorkLog,
+      ),
     );
   }
 }
