4e3d618467332245324517aba4501a8fcd557efd
Amindu Liyanaratne <reservedchery@gmail.com>
2025-09-12 23:23:41 +0530

    added network interceptor for adding authorization header

---
+++ lib/core/services/network/network_interceptor.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_time_tracker/data/models/jira_auth/jira_auth_model.dart';
+import 'package:flutter_time_tracker/domain/repositories/i_jira_auth_repository.dart';
+
+class NetworkInterceptor extends Interceptor {
+  final IJiraAuthRepository _jiraAuthRepository;
+
+  NetworkInterceptor(this._jiraAuthRepository);
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
+    JiraAuthModel jiraAuthModel = await _jiraAuthRepository.read();
+
+    options.baseUrl = "https://${jiraAuthModel.workspace}";
+    options.headers['content-type'] = 'application/json';
+    options.headers['accept'] = 'application/json';
+
+    if (jiraAuthModel.apiToken != "") {
+      options.headers['Authorization'] = "Basic ${_jiraAuthRepository.getAccessToken()}";
+    }
+
+    super.onRequest(options, handler);
+  }
+}
