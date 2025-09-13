f37ece6d3f37a8aa653e6f4bf2a1e4afd6741173
Amindu Liyanaratne <reservedchery@gmail.com>
2025-09-13 22:49:56 +0530

    added providers for mapper repository and service

--- lib/core/DI/repository_providers.dart
+++ lib/core/DI/repository_providers.dart
@@ -2,9 +2,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_time_tracker/core/DI/source_providers.dart';
 import 'package:flutter_time_tracker/core/services/network/network_service_provider.dart';
 import 'package:flutter_time_tracker/data/repositories/jira_auth_repository.dart';
+import 'package:flutter_time_tracker/data/repositories/jira_work_log_repository.dart';
 import 'package:flutter_time_tracker/data/repositories/work_log_repository.dart';
 import 'package:flutter_time_tracker/domain/repositories/i_jira_auth_repository.dart';
+import 'package:flutter_time_tracker/domain/repositories/i_jira_work_log_repository.dart';
 import 'package:flutter_time_tracker/domain/repositories/i_work_log_repository.dart';
+
+import 'mapper_providers.dart';
 
 final jiraAuthRepositoryProvider = Provider<IJiraAuthRepository>((ref) {
   return JiraAuthRepository(
@@ -15,4 +19,11 @@ final jiraAuthRepositoryProvider = Provider<IJiraAuthRepository>((ref) {
 
 final workLogRepositoryProvider = Provider<IWorkLogRepository>((ref) {
   return WorkLogRepository(ref.watch(databaseProvider));
+});
+
+final jiraWorkLogRepositoryProvider = Provider<IJiraWorkLogRepository>((ref) {
+  return JiraWorkLogRepository(
+    ref.watch(jiraApiServiceProvider),
+    ref.watch(jiraWorkLogMapper),
+  );
 });
