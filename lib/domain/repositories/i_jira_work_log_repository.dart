0062670e7d0454f24a23e3b6e560fe29c8638f12
Amindu Liyanaratne <reservedchery@gmail.com>
2025-09-13 21:50:28 +0530

    changed return type

--- lib/domain/repositories/i_jira_work_log_repository.dart
+++ lib/domain/repositories/i_jira_work_log_repository.dart
@@ -2,11 +2,11 @@ import 'package:flutter_time_tracker/data/dto/jira_work_log/jira_work_log_respon
 import 'package:flutter_time_tracker/domain/entities/work_log.dart';
 
 abstract interface class IJiraWorkLogRepository {
-  Future<JiraWorkLogModel> createJiraWorkLog(WorkLog workLog);
+  Future<JiraWorkLogResponseDto> createJiraWorkLog(WorkLog workLog);
 
-  Future<JiraWorkLogModel> updateJiraWorkLog(WorkLog workLog);
+  Future<JiraWorkLogResponseDto> updateJiraWorkLog(WorkLog workLog);
 
   Future<bool> deleteJiraWorkLog(WorkLog workLog);
 
-  Future<JiraWorkLogModel> getJiraWorkLog(WorkLog workLog);
+  Future<JiraWorkLogResponseDto> getJiraWorkLog(WorkLog workLog);
 }
