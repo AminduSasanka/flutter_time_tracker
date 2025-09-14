0393c16850e0783e3c39d116acbd5696d742bc02
Amindu Liyanaratne <reservedchery@gmail.com>
2025-09-14 19:52:02 +0530

    fixed errors are not catching

--- lib/application/services/jira_work_log_service.dart
+++ lib/application/services/jira_work_log_service.dart
@@ -40,9 +40,11 @@ class JiraWorkLogService implements IJiraWorkLogService {
       await _workLogRepository.update(updatedWorkLog);
 
       return Result.success(updatedWorkLog);
-    } on Failure catch (e) {
-      return Result.error(e);
     } catch (e, s) {
+      if (e is Failure) {
+        return Result.error(e);
+      }
+
       return Result.error(
         UnknownFailure(
           exception: e is Exception ? e : Exception(e.toString()),
@@ -88,13 +90,19 @@ class JiraWorkLogService implements IJiraWorkLogService {
       );
 
       for (WorkLog workLog in worklogsToBeSynced) {
-        await syncWorkLog(workLog);
+        final result = await syncWorkLog(workLog);
+
+        if (result.isError()) {
+          throw result.tryGetError()!;
+        }
       }
 
       return Result.success(null);
-    } on Failure catch (e) {
-      return Result.error(e);
     } catch (e, s) {
+      if (e is Failure) {
+        return Result.error(e);
+      }
+
       return Result.error(
         UnknownFailure(
           exception: e is Exception ? e : Exception(e.toString()),
