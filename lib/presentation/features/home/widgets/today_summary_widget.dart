5f93364ce6efd4941afca5379abccd514b2ab59c
Amindu Liyanaratne <reservedchery@gmail.com>
2025-09-22 23:41:57 +0530

    added summary to home page

--- lib/presentation/features/home/widgets/today_summary_widget.dart
+++ lib/presentation/features/home/widgets/today_summary_widget.dart
@@ -1,55 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_time_tracker/core/DI/controller_providers.dart';
 import 'package:flutter_time_tracker/core/theme/text_styles.dart';
+import 'package:flutter_time_tracker/presentation/shared/helpers/duration_to_spent_time.dart';
 
 class TodaySummary extends ConsumerWidget {
   const TodaySummary({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text('Today Summary', style: TextStyles.title),
-        SizedBox(height: 12),
-        Card(
-          shape: RoundedRectangleBorder(
-            side: const BorderSide(color: Colors.black12, width: 1.0),
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          elevation: 0,
-          child: Padding(
-            padding: const EdgeInsetsGeometry.all(8),
-            child: Row(
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.4,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Hours", style: TextStyles.subTitle),
-                      SizedBox(height: 8),
-                      Text("08:47", style: TextStyles.title),
-                    ],
-                  ),
+    final summaryState = ref.watch(homePageControllerProvider);
+
+    return summaryState.when(
+      data: (state) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Today Summary', style: TextStyles.title),
+            SizedBox(height: 12),
+            Card(
+              shape: RoundedRectangleBorder(
+                side: const BorderSide(color: Colors.black12, width: 1.0),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              elevation: 0,
+              child: Padding(
+                padding: const EdgeInsetsGeometry.directional(
+                  start: 16,
+                  end: 16,
+                  top: 16,
+                  bottom: 16,
                 ),
-                Spacer(),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.4,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Tasks", style: TextStyles.subTitle),
-                      SizedBox(height: 8),
-                      Text("23", style: TextStyles.title),
-                    ],
-                  ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("Hours", style: TextStyles.subTitle),
+                              SizedBox(height: 8),
+                              Text(
+                                durationToSpentTime(state.todayHours),
+                                style: TextStyles.title,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Spacer(),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("Tasks", style: TextStyles.subTitle),
+                              SizedBox(height: 8),
+                              Text(
+                                state.todayTasksCount.toString(),
+                                style: TextStyles.title,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ],
+          ],
+        );
+      },
+      error: (error, stack) => Text('Error: $error'),
+      loading: () => Center(child: const CircularProgressIndicator()),
     );
   }
 }
