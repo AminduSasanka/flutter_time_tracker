3a38f631c0b9a27ed75e41e853a84ec121ff7dc4
Amindu Liyanaratne <reservedchery@gmail.com>
2025-09-22 23:39:28 +0530

    modified home page state with summary attributes

--- lib/presentation/features/home/state/home_page_state.dart
+++ lib/presentation/features/home/state/home_page_state.dart
@@ -1,13 +1,57 @@
 class HomePageState {
-  final bool isLoading;
+  final Duration todayHours;
+  final int todayTasksCount;
+  final Duration weekHours;
+  final int weekTasksCount;
+  final Duration monthHours;
+  final int monthTasksCount;
+  final bool isError;
+  final String errorMessage;
 
-  HomePageState(this.isLoading);
+  HomePageState({
+    required this.todayHours,
+    required this.todayTasksCount,
+    required this.weekHours,
+    required this.weekTasksCount,
+    required this.monthHours,
+    required this.monthTasksCount,
+    required this.isError,
+    required this.errorMessage,
+  });
 
   factory HomePageState.initial() {
-    return HomePageState(false);
+    return HomePageState(
+      todayHours: Duration.zero,
+      todayTasksCount: 0,
+      weekHours: Duration.zero,
+      weekTasksCount: 0,
+      monthHours: Duration.zero,
+      monthTasksCount: 0,
+      isError: false,
+      errorMessage: '',
+    );
   }
 
-  HomePageState copyWith(bool? isLoading) {
-    return HomePageState(isLoading ?? this.isLoading);
+  HomePageState copyWith({
+    bool? isLoading,
+    Duration? todayHours,
+    int? todayTasksCount,
+    Duration? weekHours,
+    int? weekTasksCount,
+    Duration? monthHours,
+    int? monthTasksCount,
+    bool? isError,
+    String? errorMessage,
+  }) {
+    return HomePageState(
+      todayHours: todayHours ?? this.todayHours,
+      todayTasksCount: todayTasksCount ?? this.todayTasksCount,
+      weekHours: weekHours ?? this.weekHours,
+      weekTasksCount: weekTasksCount ?? this.weekTasksCount,
+      monthHours: monthHours ?? this.monthHours,
+      monthTasksCount: monthTasksCount ?? this.monthTasksCount,
+      isError: isError ?? this.isError,
+      errorMessage: errorMessage ?? this.errorMessage,
+    );
   }
 }
