c98a34f0c42f682c07abddad54e5403df94b5792
Amindu Liyanaratne <reservedchery@gmail.com>
2025-09-22 23:39:58 +0530

    added home page controller and provider

--- lib/presentation/features/home/controller/home_page_controller.dart
+++ lib/presentation/features/home/controller/home_page_controller.dart
@@ -1,9 +1,67 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_time_tracker/presentation/features/home/state/home_page_state.dart';
+import 'dart:async';
 
-class HomePageController extends AutoDisposeNotifier<HomePageState> {
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_time_tracker/core/DI/service_providers.dart';
+import 'package:flutter_time_tracker/domain/entities/work_log.dart';
+import 'package:flutter_time_tracker/presentation/features/home/state/home_page_state.dart';
+import 'package:flutter_time_tracker/presentation/shared/helpers/spent_time_to_duration.dart';
+
+class HomePageController extends AsyncNotifier<HomePageState> {
   @override
-  HomePageState build() {
-    return HomePageState.initial();
+  FutureOr<HomePageState> build() async {
+    final todayResult = await ref
+        .read(workLogServiceProvider)
+        .getTodayWorkLogs();
+
+    if (todayResult.isError()) {
+      return HomePageState(
+        todayHours: Duration.zero,
+        todayTasksCount: 0,
+        weekHours: Duration.zero,
+        weekTasksCount: 0,
+        monthHours: Duration.zero,
+        monthTasksCount: 0,
+        isError: true,
+        errorMessage: todayResult.tryGetError()!.message,
+      );
+    }
+
+    final todayWorkLogs = todayResult.tryGetSuccess();
+
+    if (todayWorkLogs!.isEmpty) {
+      return HomePageState(
+        todayHours: Duration.zero,
+        todayTasksCount: 0,
+        weekHours: Duration.zero,
+        weekTasksCount: 0,
+        monthHours: Duration.zero,
+        monthTasksCount: 0,
+        isError: true,
+        errorMessage: "",
+      );
+    }
+
+    return HomePageState(
+      todayHours: _totalHours(todayWorkLogs),
+      todayTasksCount: todayWorkLogs.length,
+      weekHours: Duration.zero,
+      weekTasksCount: 0,
+      monthHours: Duration.zero,
+      monthTasksCount: 0,
+      isError: true,
+      errorMessage: todayResult.tryGetError()!.message,
+    );
+  }
+
+  Duration _totalHours(List<WorkLog> workLogs) {
+    Duration totalHours = Duration.zero;
+
+    for (final workLog in workLogs) {
+      if (workLog.timeSpent != null) {
+        totalHours += spentTimeToDuration(workLog.timeSpent!);
+      }
+    }
+
+    return totalHours;
   }
 }
