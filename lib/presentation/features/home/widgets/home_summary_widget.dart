5dccde0c3b69e9f2346e2de188a22ff0bc15df31
Amindu Liyanaratne <reservedchery@gmail.com>
2025-09-23 23:17:10 +0530

    renamed today summary to home summary widget and added all summary widgets

---
+++ lib/presentation/features/home/widgets/home_summary_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_time_tracker/core/DI/controller_providers.dart';
+import 'package:flutter_time_tracker/core/theme/text_styles.dart';
+import 'package:flutter_time_tracker/presentation/features/home/widgets/summary_widget.dart';
+import 'package:flutter_time_tracker/presentation/shared/helpers/duration_to_spent_time.dart';
+
+class HomeSummaryWidget extends ConsumerWidget {
+  const HomeSummaryWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final summaryState = ref.watch(homePageControllerProvider);
+
+    return summaryState.when(
+      data: (state) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SummaryWidget(
+              title: "Today's Summary",
+              hours: state.todayHours,
+              tasksCount: state.todayTasksCount,
+            ),
+            SizedBox(height: 18),
+            SummaryWidget(
+              title: "This Week's Summary",
+              hours: state.weekHours,
+              tasksCount: state.weekTasksCount,
+            ),
+            SizedBox(height: 18),
+            SummaryWidget(
+              title: "This Month's Summary",
+              hours: state.monthHours,
+              tasksCount: state.monthTasksCount,
+            ),
+          ],
+        );
+      },
+      error: (error, stack) => Text('Error: $error, Stack: $stack '),
+      loading: () => Center(child: const CircularProgressIndicator()),
+    );
+  }
+}
