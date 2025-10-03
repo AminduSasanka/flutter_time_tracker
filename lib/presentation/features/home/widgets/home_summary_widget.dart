310b4974197e60b3da90eaa84e275549349807a5
Amindu Liyanaratne <reservedchery@gmail.com>
2025-10-03 22:55:29 +0530

    moved summary widget to a single file

--- lib/presentation/features/home/widgets/home_summary_widget.dart
+++ lib/presentation/features/home/widgets/home_summary_widget.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_time_tracker/core/DI/controller_providers.dart';
-import 'package:flutter_time_tracker/presentation/features/home/widgets/summary_widget.dart';
+import 'package:flutter_time_tracker/core/theme/text_styles.dart';
+import 'package:flutter_time_tracker/presentation/shared/helpers/duration_to_spent_time.dart';
 
 class HomeSummaryWidget extends ConsumerWidget {
   const HomeSummaryWidget({super.key});
@@ -15,22 +16,71 @@ class HomeSummaryWidget extends ConsumerWidget {
         return Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SummaryWidget(
-              title: "Today Summary",
-              hours: state.todayHours,
-              tasksCount: state.todayTasksCount,
-            ),
-            SizedBox(height: 18),
-            SummaryWidget(
-              title: "Week Summary",
-              hours: state.weekHours,
-              tasksCount: state.weekTasksCount,
-            ),
-            SizedBox(height: 18),
-            SummaryWidget(
-              title: "Month Summary",
-              hours: state.monthHours,
-              tasksCount: state.monthTasksCount,
+            Row(
+              children: [
+                Expanded(
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    elevation: 0,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 16,
+                        right: 8,
+                        bottom: 16,
+                        left: 8,
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            "Hours",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Theme.of(context).colorScheme.onSurface,
+                            ),
+                          ),
+                          Text(
+                            durationToSpentTime(state.todayHours),
+                            style: TextStyles.title,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    elevation: 0,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 16,
+                        right: 8,
+                        bottom: 16,
+                        left: 8,
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            "Tasks",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Theme.of(context).colorScheme.onSurface,
+                            ),
+                          ),
+                          Text(
+                            state.todayTasksCount.toString(),
+                            style: TextStyles.title,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         );
