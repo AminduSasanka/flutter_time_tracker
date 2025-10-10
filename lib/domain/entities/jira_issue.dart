1d16d876b98953e5ed35434e849f6ccbb047c0a7
Amindu Liyanaratne <reservedchery@gmail.com>
2025-10-11 00:21:01 +0530

    added jira issue entity

---
+++ lib/domain/entities/jira_issue.dart
@@ -0,0 +1,17 @@
+class JIraIssue {
+  final int id;
+  final String img;
+  final String key;
+  final String keyHtml;
+  final String summary;
+  final String summaryText;
+
+  JIraIssue({
+    required this.id,
+    required this.img,
+    required this.key,
+    required this.keyHtml,
+    required this.summary,
+    required this.summaryText,
+  });
+}
