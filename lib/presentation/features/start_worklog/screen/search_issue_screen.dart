bbdf5e1c2c3c5d6b153286aa47430b947e2ab5eb
Amindu Liyanaratne <reservedchery@gmail.com>
2025-10-11 02:36:08 +0530

    changed back to stateless widget

--- lib/presentation/features/start_worklog/screen/search_issue_screen.dart
+++ lib/presentation/features/start_worklog/screen/search_issue_screen.dart
@@ -1,36 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_time_tracker/core/DI/controller_providers.dart';
 import 'package:flutter_time_tracker/core/theme/text_styles.dart';
+import 'package:flutter_time_tracker/presentation/features/start_worklog/widgets/search_result_list_widget.dart';
 import 'package:flutter_time_tracker/presentation/features/start_worklog/widgets/search_widget.dart';
 
-class SearchIssueScreen extends ConsumerWidget {
+class SearchIssueScreen extends StatelessWidget {
   const SearchIssueScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final searchScreenController = ref.watch(
-      searchIssueScreenControllerProvider,
-    );
-
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Search Jira Issue', style: TextStyles.title)),
-      body: searchScreenController.when(
-        data: (state) {
-          return SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.only(
-                top: 16,
-                left: 8,
-                right: 8,
-                bottom: 16,
-              ),
-              child: SearchWidget(),
-            ),
-          );
-        },
-        error: (error, stackTrace) => Center(child: Text(error.toString())),
-        loading: () => const Center(child: CircularProgressIndicator()),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 16,
+            left: 8,
+            right: 8,
+            bottom: 16,
+          ),
+          child: Wrap(children: [
+            SearchWidget(),
+            SearchResultListWidget(),
+          ]),
+        ),
       ),
     );
   }
